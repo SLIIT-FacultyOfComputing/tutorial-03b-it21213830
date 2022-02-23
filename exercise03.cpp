f92176f8d223684a96e633262d50a88734e60564
haritha <99177404+it21213830@users.noreply.github.com>
2022-02-23 14:25:25 +0000

    ex 03 complete

--- exercise03.cpp
+++ exercise03.cpp
@@ -1,43 +1,36 @@
-#include <iostream>
-
+ #include <iostream>
 using namespace std;
+int volume(int height,int width,int length);
 
-int volume(int height, int width, int length);
-struct box {
-   int height;
-   int width;
-   int lenght;
-}box1,box2;
 int main() {
+
+    int box1Height,box1Width ,box1Length;
+    int box2Height,box2Width ,box2Length;
+    int totalVolume, totalSurface;
+
+    cout<<"Enter Box 1 Height : ";
+    cin>>box1Height;
+    cout<<"Enter Box 1 Width : ";
+    cin>>box1Width;
+    cout<<"Enter Box 1 Length : ";
+    cin>>box1Length;
+    
+    cout<<"Enter Box 2 Height : ";
+    cin>>box2Height;
+    cout<<"Enter Box 2 Width : ";
+    cin>>box2Width;
+    cout<<"Enter Box 2 Length : ";
+    cin>>box2Length;
+
+    totalVolume = volume(box1Height, box1Width, box1Length)
+ + volume(box2Height, box2Width, box2Length);
  
-    int totalVolume;
-    
+ cout << "Volume of Box is : " << totalVolume << endl;
 
-    cout << "Enter Box 1 Height : ";
-    cin >> box1.height;
-    cout << "Enter Box 1 Width : ";
-    cin >> box1.width;
-    cout << "Enter Box 1 Length : ";
-    cin >> box1.lenght;
-    
-    cout << "Enter Box 2 Height : ";
-    cin >> box2.height;
-    cout << "Enter Box 2 Width : ";
-    cin >> box2.width;
-    cout << "Enter Box 2 Length : ";
-    cin >>box2.lenght;
-    
-    
-    totalVolume = volume( box1.height,box1.width,box1.lenght)
-             + volume(box2.height ,box2.width ,box2.lenght );
-    
-    cout << "Volume of Box is: " << totalVolume << endl;
-    
-    return 0;
+ return 0;
+
 }
-int volume(int height, int width, int length)
-{
-  return height * width * lenght;
+int volume(int height,int width,int length){
+
+    return height*width*length;
 }
-
-
